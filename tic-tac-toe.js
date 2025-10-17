52b0764754f59c7fca193b62f6cf07361814c2ca
Sila678 <silanaris24@gmail.com>
2025-10-17 13:58:14 -0500

    Exercise 2 addin X or O

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,4 @@
-use strict;
+//use strict;
 
 window.onload = function(){
 
@@ -16,6 +16,27 @@ window.onload = function(){
 	for (var i = 0; i < sqrs.length; i++){
 		sqrs[i].setAttribute("class", "square");
 	};
+
+	//Game play
+	//Tracks the current player
+	let curtPlayer = "X";
+
+	//Keeps track of game state
+	let gameState = Array(9).fill(null);
+
+	for (let i=0; i< sqrs.length; i++){
+		sqrs[i].addEventListener("click", function(){
+			//Only mark a square if empty
+			if (sqrs[i].textContent === ""){
+				sqrs[i].textContent = curtPlayer;
+				sqrs[i].classList.add(curtPlayer); //for appropriate colour from stylesheet
+				gameState[i] = curtPlayer;
+
+				//Switch turn
+				curtPlayer = curtPlayer === "X" ? "O" : "X";
+			}
+		});
+	}
 
 
 }
